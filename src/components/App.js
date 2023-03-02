bd6802c1562511f7dd0662e960be6b105856609b
Suraj-Accio <124239927+Suraj-Accio@users.noreply.github.com>
2023-03-02 16:21:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
+import UserForm from "./UserForm";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <UserForm />
     </div>
   )
 }
